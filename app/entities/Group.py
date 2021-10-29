4bde6639c328b3a75fae649305dbf987206b6e38
carneirofc <claudiofcarneiro@hotmail.com>
2021-10-29 10:57:23 -0300

    group/ isort, include group id

--- app/entities/Group.py
+++ app/entities/Group.py
@@ -2,12 +2,18 @@ import threading
 
 
 class Group:
-    """ Group of PVs """
+    """Group of PVs"""
 
-    def __init__(self, name: str, enabled: bool = True):
-        self.name: str = name
+    def __init__(self, id: str, name: str, enabled: bool = True, description: str = ""):
         self._enabled: bool = enabled
+        self._id = id
+        self.description = description
         self.lock = threading.RLock()
+        self.name: str = name
+
+    @property
+    def id(self):
+        return self._id
 
     @property
     def enabled(self):
@@ -20,7 +26,12 @@ class Group:
             self._enabled = value
 
     def __str__(self):
-        return f'<Group="{self.name}" enabled={self.enabled}>'
+        return f'Group(id={self._id},name="{self.name}",enabled="{self.enabled}")'
 
     def as_dict(self):
-        return {"name": self.name, "enabled": self.enabled}
+        return {
+            "name": self.name,
+            "enabled": self.enabled,
+            "id": self.id,
+            "description": self.description,
+        }
